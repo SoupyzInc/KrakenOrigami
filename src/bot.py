2fa40acab3d6f34b8062cdb3b25d8575412e6cd9
Matthew Okashita <okashita.matthew@gmail.com>
2021-03-16 01:58:37 -0400

    Replaced SMA with EMA
    Replaced the incorrect SMA method with a working EMA method. The new EMA method is only off by a few cents compared to Kraken.

--- src/bot.py
+++ src/bot.py
@@ -8,44 +8,41 @@ from discord.ext import commands
 from datetime import datetime
 from pairs import quotes, bases, pair, base_urls, base_colors
 
-### NOTE: SMA CALCULATIONS ARE WRONG AND NEED TO BE FIXED. 
-# CALCULATIONS ARE HIGHER THAN KRAKEN SMA. ###
-def sma(period, value, data):
+def ema(period, value, data):
     """
-    Return the SMA of the given data for a given period.
+    Returns the EMA of the given data for a given period.
 
     Args:
         period: The time period or interval, in minutes, to be used.
         value: Which value from the OHLC data to be averaged. Values are as follows:
           1 - time, 2 - topen, 2 - high, 3 - low, 4 - close, 5 - vwap, 6 - volume, 7 - count
-        data: The data set to be used. Expects the Kraken API's OHLC data array. 
+        data: The data set to be used. Expects the Kraken API's OHLC data array.
           Ex: data = k.query_public('OHLC', {'pair': XLTCZUSD, 'interval': '60'})['result'][XLTCZUSD] 
         
     Returns:
-        An array of the simple moving average.
+        An array of the exponential moving average.
     """
+    
+    k = 2 / (period + 1)
+    EMA = []
+    i = 1
+    sum = 0
+    while i < period:
+        sum += float(data[-i][value])
+        i += 1
 
-    loc = 0
-    offset = 0
+    sum = sum / period
+    EMA.append(sum)
 
-    sum = float(0)
-    SMA = []
+    i = period + 1 # Offset EMA for initial SMA calculation
+    while i < len(data):
+        EMA.append(float(data[i][value]) * k + float(EMA[i - (period + 1)]) * (1 - k))
+        i += 1
 
-    while loc < len(data): # Iterate all values
-        if loc == (period + offset):
-            SMA.append(sum / period) 
+    return EMA
 
-            offset += 1
-            loc = offset  
-            sum = 0
-
-            loc = loc - 1
-        else:
-            sum += float(data[loc][value]) # Sum closing values
-            loc += 1
-
-    return SMA
-
+### CREATE A SPAM PREVENTION SYSTEM SO AS TO REMAIN UNDER THE API RATE LIMIT. ###
+### ADD ERROR HANDLING WITH INVALID USER INPUTS ###
 def main():
     """
     The main method that runs the Discord bot.
@@ -67,19 +64,35 @@ def main():
     ### HELP COMMANDS ### 
     @bot.command(name = 'help')
     async def documenatation(ctx, type = ''):
-        if type == '':
-            embed = discord.Embed(title = 'Kraken Origami Documentation', 
-                            description = '`.help register`\n`.help info`', color = 0x5741d9)
-        elif type == 'register':
+        if type == 'register':
             embed = discord.Embed(title = '.register Documentation', 
-                            description = 'Registers an account to begin paper trading.', color = 0x5741d9)
+                            description = '`.register`\n Registers an account to begin paper trading.', color = 0x5741d9)
         elif type == 'info':
             embed = discord.Embed(title = '.info Documentation',
-                            description = '`.info <Base> <Quote>`\n Returns info from the Kraken API on' +
-                            ' the specified pair.', color = 0x5741d9)
-        
+                            description = '`.info <Base> <Quote>`\n Returns info from the Kraken API on the specified pair.', color = 0x5741d9)
+        elif type == 'pairs':
+            embed = discord.Embed(title = '.pairs Documentation',
+                            description = '`.pairs`\n Returns all possible pairs in two messages.', color = 0x5741d9)
+        else:
+            embed = discord.Embed(title = 'Kraken Origami Documentation', 
+                            description = '`.help register`\n`.help info`\n`.help pairs`', color = 0x5741d9)
+
         embed.set_thumbnail(url = bot.user.avatar_url)
         embed.set_footer(text = 'Requested by ' + ctx.author.name + '.', icon_url = ctx.author.avatar_url)
+        await ctx.send(embed = embed)
+
+    @bot.command(name = 'pairs')
+    async def _pairs(ctx):
+        embed = discord.Embed(title = 'Pairs 1/2', color = 0x5741d9,
+                        description = 'AAVEAUD, AAVEETH, AAVEEUR, AAVEGBP, AAVEUSD, AAVEXBT, ADAAUD, ADAETH, ADAEUR, ADAGBP, ADAUSD, ADAUSDT, ADAXBT, ALGOETH, ALGOEUR, ALGOGBP, ALGOUSD, ALGOXBT, ANTETH, ANTEUR, ANTUSD, ANTXBT, ATOMAUD, ATOMETH, ATOMEUR, ATOMGBP, ATOMUSD, ATOMXBT, BALETH, BALEUR, BALUSD, BALXBT, BATETH, BATEUR, BATUSD, BATXBT, BCHAUD, BCHETH, BCHEUR, BCHGBP, BCHJPY, BCHUSD, BCHUSDT, BCHXBT, COMPETH, COMPEUR, COMPUSD, COMPXBT, CRVETH, CRVEUR, CRVUSD, CRVXBT, DAIEUR, DAIUSD, DAIUSDT, DASHEUR, DASHUSD, DASHXBT, DOTAUD, DOTETH, DOTEUR, DOTGBP, DOTUSD, DOTUSDT, DOTXBT, EOSETH, EOSEUR, EOSUSD, EOSUSDT, EOSXBT, ETHAUD, ETHCHF, ETHDAI, ETHUSDC, ETHUSDT, EURAUD, EURCAD, EURCHF, EURGBP, EURJPY, EWTEUR, EWTGBP, EWTUSD, EWTXBT, FILAUD, FILETH, FILEUR, FILGBP, FILUSD, FILXBT, FLOWETH, FLOWEUR, FLOWGBP, FLOWUSD, FLOWXBT, GNOETH, GNOEUR, GNOUSD, GNOXBT, GRTAUD, GRTETH, GRTEUR, GRTGBP, GRTUSD, GRTXBT, ICXETH, ICXEUR, ICXUSD, ICXXBT, KAVAETH, KAVAEUR, KAVAUSD, KAVAXBT, KEEPETH, KEEPEUR, KEEPUSD, KEEPXBT, KNCETH, KNCEUR, KNCUSD, KNCXBT, KSMAUD, KSMETH, KSMEUR, KSMGBP, KSMUSD, KSMXBT, LINKAUD, LINKETH, LINKEUR, LINKGBP, LINKUSD, LINKUSDT, LINKXBT, LSKETH, LSKEUR, LSKUSD, LSKXBT, LTCAUD, LTCETH, LTCGBP, LTCUSDT, MANAETH, MANAEUR, MANAUSD,')
+        embed.set_thumbnail(url = bot.user.avatar_url)
+        embed.set_footer(text = 'Requested by ' + ctx.author.name + '.', icon_url = ctx.author.avatar_url)
+        await ctx.send(embed = embed)
+
+        embed = discord.Embed(title = 'Pairs 2/2', color = 0x5741d9,
+                        description = 'MANAXBT, NANOETH, NANOEUR, NANOUSD, NANOXBT, OCEANEUR, OCEANGBP, OCEANUSD, OCEANXBT, OMGETH, OMGEUR, OMGUSD, OMGXBT, OXTETH, OXTEUR, OXTUSD, OXTXBT, PAXGETH, PAXGEUR, PAXGUSD, PAXGXBT, QTUMETH, QTUMEUR, QTUMUSD, QTUMXBT, REPVETH, REPVEUR, REPVUSD, REPVXBT, SCETH, SCEUR, SCUSD, SCXBT, SNXAUD, SNXETH, SNXEUR, SNXGBP, SNXUSD, SNXXBT, STORJETH, STORJEUR, STORJUSD, STORJXBT, TBTCETH, TBTCEUR, TBTCUSD, TBTCXBT, TRXETH, TRXEUR, TRXUSD, TRXXBT, UNIETH, UNIEUR, UNIUSD, UNIXBT, USDCAUD, USDCEUR, USDCGBP, USDCHF, USDCUSD, USDCUSDT, USDTAUD, USDTCAD, USDTCHF, USDTEUR, USDTGBP, USDTJPY, USDTUSD, WAVESETH, WAVESEUR, WAVESUSD, WAVESXBT, XBTAUD, XBTCHF, XBTDAI, XBTUSDC, XBTUSDT, XDGEUR, XDGUSD, XETCETH, XETCXBT, XETCEUR, XETCUSD, XETHXBT, XETHCAD, XETHEUR, XETHGBP, XETHJPY, XETHUSD, XLTCXBT, XLTCEUR, XLTCJPY, XLTCUSD, XMLNETH, XMLNXBT, XMLNEUR, XMLNUSD, XREPETH, XREPXBT, XREPEUR, XREPUSD, XRPAUD, XRPETH, XRPGBP, XRPUSDT, XTZAUD, XTZETH, XTZEUR, XTZGBP, XTZUSD, XTZXBT, XXBTCAD, XXBTEUR, XXBTGBP, XXBTJPY, XXBTUSD, XXDGXBT, XXLMXBT, XXLMAUD, XXLMEUR, XXLMGBP, XXLMUSD, XXMRXBT, XXMREUR, XXMRUSD, XXRPXBT, XXRPCAD, XXRPEUR, XXRPJPY, XXRPUSD, XZECXBT, XZECEUR, XZECUSD, YFIAUD, YFIETH, YFIEUR, YFIGBP, YFIUSD, YFIXBT')
+        embed.set_thumbnail(url = bot.user.avatar_url)
+        embed.set_footer(text = 'Requested by ' + ctx.author.name + '.', icon_url = ctx.author.avatar_url)     
         await ctx.send(embed = embed)
 
     ### ECONOMY COMMANDS ###
@@ -119,28 +132,30 @@ def main():
                                       # Get most recent price
         embed = discord.Embed(title = data[-1][4] + ' | ' + crypto, color = base_colors[base])
         embed.timestamp = datetime.utcnow().replace(tzinfo=pytz.utc)
-        embed.add_field(name = 'SMA', value = '10 SMA | ' + str(round(sma(10, 4, data)[-1], 2)) +
-                                            '\n20 SMA | ' + str(round(sma(20, 4, data)[-1], 2)) +
-                                            '\n30 SMA | ' + str(round(sma(30, 4, data)[-1], 2)) +
-                                            '\n40 SMA | ' + str(round(sma(40, 4, data)[-1], 2)))
         
+        embed.add_field(name = 'EMA', value = '10 EMA | ' + str(round(ema(10, 4, data)[-1], 2)) +
+                                            '\n20 EMA | ' + str(round(ema(20, 4, data)[-1], 2)) +
+                                            '\n30 EMA | ' + str(round(ema(30, 4, data)[-1], 2)) +
+                                            '\n40 EMA | ' + str(round(ema(40, 4, data)[-1], 2)))
+
         # Below red cloud
-        if float(data[-1][4]) < sma(40, 4, data)[-1]: 
-            ta = 'Possible entry point.'
+        if float(data[-1][4]) < ema(40, 4, data)[-1]: 
+            emaText = 'Possible entry point.'
         # In red cloud
-        elif (float(data[-1][4]) >= sma(40, 4, data)[-1]) & (float(data[-1][4]) < sma(30, 4, data)[-1]):
-            ta = 'Possible entry point or close position.'
+        elif (float(data[-1][4]) >= ema(40, 4, data)[-1]) & (float(data[-1][4]) < ema(30, 4, data)[-1]):
+            emaText = 'Possible entry point or close position.'
         # Between clouds
-        elif (float(data[-1][4]) >= sma(30, 4, data)[-1]) & (float(data[-1][4]) < sma(20, 4, data)[-1]):
-            ta = 'Possible entry point or (prepare) to close position.' 
+        elif (float(data[-1][4]) >= ema(30, 4, data)[-1]) & (float(data[-1][4]) < ema(20, 4, data)[-1]):
+            emaText = 'Possible entry point or (prepare to) close position.' 
         # In green cloud
-        elif (float(data[-1][4]) >= sma(20, 4, data)[-1]) & (float(data[-1][4]) < sma(10, 4, data)[-1]):
-            ta = 'Continue to hold.' 
+        elif (float(data[-1][4]) >= ema(20, 4, data)[-1]) & (float(data[-1][4]) < ema(10, 4, data)[-1]):
+            emaText = 'Continue to hold.' 
         # Above green cloud
         else:
-            ta = 'Continue to hold or close position.'
+            emaText = 'Continue to hold or close position.'
         
-        embed.add_field(name = 'TA', value = ta)
+        embed.set_footer(text = 'Requested by ' + ctx.author.name, icon_url = ctx.author.avatar_url) 
+        embed.add_field(name = 'TA', value = emaText)
         embed.set_thumbnail(url = base_urls[base])
 
         await ctx.send(embed = embed)
