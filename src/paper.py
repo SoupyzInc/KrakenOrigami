1b40dac0244348d3d9c1f45ba1a9ed3aec211de0
Matthew Okashita <okashita.matthew@gmail.com>
2021-03-18 12:06:12 -0400

    Added SQL interaction for close and account methods

--- src/paper.py
+++ src/paper.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import pytz
+import krakenex
+import discord
+from ta import convert, get_ohlc
+from pairs import base_urls
 from datetime import datetime
 
 db = mysql.connector.connect(
-  host="localhost",
-  user="Okashita",
-  password="L+E+7b__?3eK3&L#cCRsK9F",
-  database="accounts"
 )
 
 def register(id):
@@ -15,9 +16,9 @@ def register(id):
     Args:
         id: The Discord user id of the user to be added.
     """
-    
+
     print(datetime.utcnow().strftime("%y-%m-%d %H:%M:%S"))
-    cursor = db.cursor()
+    cursor = db.cursor(buffered=True)
     cursor.execute("INSERT INTO users (id) VALUES (%s)", (id,))
     db.commit()
 
@@ -26,13 +27,13 @@ def valid(id):
   Checks if a user has made an account or not.
 
   Args:
-      id: The Discord user id of the user to be querired.
+      id: The Discord user id of the user to be queried.
 
   Returns:
     Boolean True if the user has an account and False if not.
   """
 
-  cursor = db.cursor()
+  cursor = db.cursor(buffered=True)
   cursor.execute("SELECT id FROM users")
 
   for x in cursor:
@@ -53,10 +54,29 @@ def get_balance(id):
         A string of the user's balance.
     """
 
-    cursor = db.cursor()
+    cursor = db.cursor(buffered=True)
     cursor.execute("SELECT * FROM users WHERE id = " + str(id))
     for x in cursor:
         return x[1]
+
+def get_positions(id):
+    """
+    Gets a user's open positions.
+
+    Args:
+        id:  The Discord user id of the user to be queried.
+
+    Returns:
+        A list of trades, which are tuples in the format: (id, user_id, pair, time of trade, price of crypto at purchase, amount purchased).
+    """
+    cursor = db.cursor(buffered=True)
+    cursor.execute("SELECT * FROM open_trades WHERE user_id = " + str(id))
+
+    out = []
+    for x in cursor:
+        out.append(x)
+
+    return out
 
 def buy(id, pair, price, amount):
     """
@@ -69,7 +89,7 @@ def buy(id, pair, price, amount):
         amount: The amount of the crypto purchased (in dollars).
     """
 
-    cursor = db.cursor()
+    cursor = db.cursor(buffered=True)
 
     # Log trade
     cursor.execute("INSERT INTO open_trades (user_id, pair, price, amount) VALUES(%s, %s, %s, %s)", (id, pair, price, amount))
@@ -81,7 +101,103 @@ def buy(id, pair, price, amount):
 
     db.commit()
 
+def valid_close(user_id, trade_id):
+    cursor = db.cursor(buffered=True)
+    cursor.execute("""SELECT * FROM open_trades
+                               GROUP BY """)
+    for x in cursor:
+        for y in x:
+            print(y)
 
+def close(user_id, close, ctx): #, 
+    cursor = db.cursor(buffered=True)
+
+    # Get open positions that the user opened
+    if (len(get_positions(user_id)) > 0):
+        cursor.execute("SELECT GROUP_CONCAT(id), COUNT(user_id) c FROM open_trades GROUP BY user_id HAVING c > 1")
+
+        single = True
+        conversion = [-1]
+        for x in cursor:
+            data = x[0]
+            single = False
+
+        if single:
+            cursor.execute("SELECT id FROM open_trades WHERE user_id = " + str(user_id))
+            for x in cursor:
+                data = x
+            for x in data:
+                conversion.append(x)
+        else:
+            for x in data.split(','):
+                conversion.append(x)
+
+        if int(close) <= len(conversion) and int(close) > 0:
+            # Calculate values
+            cursor.execute("SELECT * FROM open_trades WHERE id = " + str(conversion[int(close)]))
+            for x in cursor:
+                position = x
+                # (6, 344671380412956673, 'XLTCZUSD', datetime.datetime(2021, 3, 18, 0, 26, 36), 205.46, 100.0)
+                # (id, user_id, pair, time of trade, price of crypto at purchase, amount purchased)
+                # (0,  1,       2,    3,             4,                           5)
+
+            price_close = float(get_ohlc(position[2])[4])
+            return_percent = ((price_close - position[4])/ position[4]) * 100
+            shares = position[5] / position[4]
+            return_decimal = (shares * price_close) - (shares * position[4])
+
+            # Log trade as now closed
+            cursor.execute("""INSERT INTO closed_trades (user_id, pair, time_open, price_open, price_close, amount, return_percent, return_decimal) 
+                            VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""", 
+                            (user_id, position[2], position[3], position[4], price_close, position[5], return_percent, return_decimal))
+            db.commit()
+
+            # Remove open position
+            cursor.execute("DELETE FROM open_trades WHERE id = " + str(conversion[int(close)]))
+            db.commit()
+
+            # Add profits (or lack of) to balance
+            balance = get_balance(user_id) + return_decimal
+
+            cursor.execute("""UPDATE users
+                        SET balance = """ + str(balance) +
+                    """WHERE id = """ + str(user_id))
+            db.commit()   
+
+            if return_decimal > 0:
+                color = 0x43B581
+            elif return_decimal < 0:
+                color = 0xF04747
+            else:
+                color = 0x202225
+
+            embed = discord.Embed(title = ctx.author.name + " closed " + str(position[2]), color = color)
+
+            if (return_percent > 0):
+                percent = "+" + str(round(return_percent, 2)) + "%"
+                numerical = "+" + "${:,.2f}".format(return_decimal)
+            elif (return_percent < 0):
+                percent = str(round(return_percent, 2)) + "%"
+                return_decimal *= -1
+                numerical = "-" + "${:,.2f}".format(return_decimal)
+            else:
+                percent = str(round(return_percent, 2)) + "%"
+                numerical = "${:,.2f}".format(return_decimal)
+
+            out = "\n`" + percent + "` " + numerical + "\n> ${:,.2f}".format(position[5]) + " @ " + "${:,.2f}".format(position[4]) + " on " + str(position[3])
+
+            embed.add_field(name = str(position[2]), value = str(position[4]) + " 🠒 " + str(price_close) + 
+                        "\n" + out)
+            # embed.timestamp = datetime.utcnow().replace(tzinfo=pytz.utc)
+            embed.set_footer(text = 'Closed on ' + str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+            # embed.set_thumbnail(url = base_urls[base])
+
+            return embed
+            # return 'Position `' + position[2] + ' | ' + str(position[5]) + ' @ ' + str(position[4]) + "` successfully closed."
+        else:
+            return 'You only have ' + str(len(conversion)) + ' positions opened.'
+    else:
+        return 'You do not have any positions opened. Use `.buy help` to learn how to buy crypto.'
 ### Creation of the account database tables:
 # cursor = db.cursor()
 
